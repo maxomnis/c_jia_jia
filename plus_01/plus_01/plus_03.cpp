e24a716a96db54d4c7d018788452a1cf31490467
jack zhang <maxomnis@qq.com>
2017-09-18 19:37:47 +0800

    第三章 数据处理

---
+++ plus_01/plus_01/plus_03.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+	//初始化和赋值进行的转换
+	cout.setf(ios_base::fixed, ios_base::floatfield);
+	float tree = 3;
+	int guess(3.9832);  //把int类型当做一个类，然后3.9832初始化，c11的特性
+	int debt = 7.2E12;  //超出了Int类型的最大值
+	cout << "tree" << tree << endl;  //输出3.000000
+	cout << "guess" << guess << endl; //输出3
+	cout << "debt" << debt << endl; //输出1634811904，不同编译器输出的值不一样
+	system("pause");
+	return 0;
+}
